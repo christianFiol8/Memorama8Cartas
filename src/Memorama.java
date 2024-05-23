7bbc8046794866773933baae76b7b245bcbc0b9e
Christian <fiolc5173@gmail.com>
2024-05-23 09:22:55 -0700

    Primer commit

--- src/Memorama.java
+++ src/Memorama.java
@@ -1,6 +1,12 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class Memorama {
 
@@ -34,8 +40,34 @@ public class Memorama {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 672, 699);
+		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		tablero();
+		
+	}
+	
+	public void tablero() {
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.decode("#56BDEE"));
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		panel.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.decode("#CEEE56"));
+		frame.getContentPane().add(panel_1, BorderLayout.SOUTH);
+		
+		JButton btnNewButton = new JButton("New button");
+		panel_1.add(btnNewButton);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.decode("#5A8092"));
+		frame.getContentPane().add(panel_2, BorderLayout.CENTER);
+		
 	}
 
 }
